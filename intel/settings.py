f6b3cae0759baf33f49229f842b7f893a60e5fdd
mikegyamfi <codeage20@gmail.com>
2023-12-28 19:01:03 +0000

    initial commit

--- intel/settings.py
+++ intel/settings.py
@@ -22,11 +22,11 @@ TEMPLATE_DIR = BASE_DIR / 'intel_app/templates'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config("SECRET_KEY")
-SECRET_KEY = "uhduyehudheu"
+SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = "uhduyehudheu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'www.ghbays.com'
