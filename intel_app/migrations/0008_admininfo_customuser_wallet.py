9595d1f743fbe3ab84836cf6349d468ff97602a9
mikegyamfi <codeage20@gmail.com>
2024-01-12 07:12:12 +0000

    afa

--- intel_app/migrations/0008_admininfo_customuser_wallet.py
+++ intel_app/migrations/0008_admininfo_customuser_wallet.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(blank=True, max_length=250, null=True)),
                 ('phone_number', models.BigIntegerField(blank=True, null=True)),
+                ('phone_number', models.PositiveBigIntegerField(blank=True, null=True)),
                 ('email', models.EmailField(blank=True, max_length=254, null=True)),
             ],
         ),
