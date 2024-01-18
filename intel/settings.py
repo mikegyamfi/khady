c90dfb42ee81756b1cc6bc9abbf9b14d177786c8
mikegyamfi <codeage20@gmail.com>
2024-01-18 13:31:43 +0000

    afa

--- intel/settings.py
+++ intel/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = BASE_DIR / 'intel_app/templates'
 SECRET_KEY = "uhduyehudheu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.ghbays.com',
