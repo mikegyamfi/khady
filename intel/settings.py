f4e0330531e70064c887e9d7c70a16f60aad4d6d
mikegyamfi <codeage20@gmail.com>
2024-01-19 16:52:05 +0000

    status

--- intel/settings.py
+++ intel/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = BASE_DIR / 'intel_app/templates'
 SECRET_KEY = "uhduyehudheu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'www.ghbays.com',
@@ -98,26 +98,26 @@ WSGI_APPLICATION = 'intel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': config("DATABASE_HOST"),
-        'PORT': config("DATABASE_PORT"),
-        'NAME': 'db',
-        'USER': config("DATABASE_USERNAME"),
-        'PASSWORD': config("DATABASE_PASSWORD"),
-        'OPTIONS': {
-            'sslmode': 'require'
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'HOST': config("DATABASE_HOST"),
+#         'PORT': config("DATABASE_PORT"),
+#         'NAME': 'db',
+#         'USER': config("DATABASE_USERNAME"),
+#         'PASSWORD': config("DATABASE_PASSWORD"),
+#         'OPTIONS': {
+#             'sslmode': 'require'
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
