b9a5e6d4cec6f74c74905b66467f0bf307bc2be7
mikegyamfi <codeage20@gmail.com>
2024-01-19 17:05:11 +0000

    status

--- intel_app/models.py
+++ intel_app/models.py
@@ -238,3 +238,88 @@ class TopUpRequest(models.Model):
     status = models.BooleanField(default=False, blank=False, null=False)
     date = models.DateTimeField(auto_now_add=True)
     credited_at = models.DateTimeField(auto_now_add=True)
+
+
+####################################################################################
+class Brand(models.Model):
+    name = models.CharField(max_length=250, null=False, blank=False, default="Generic")
+    description = models.CharField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    slug = models.CharField(max_length=250, null=False, blank=False)
+    name = models.CharField(max_length=250, null=False, blank=True)
+    image = models.ImageField(upload_to='category/', null=True, blank=True)
+    description = models.TextField(max_length=600, null=False, blank=False)
+    status = models.BooleanField(default=False, help_text="0=default, 1=Hidden")
+    trending = models.BooleanField(default=False, help_text="0=default, 1=Trending")
+    meta_title = models.CharField(max_length=150, null=False, blank=False)
+    meta_keywords = models.CharField(max_length=150, null=False, blank=False)
+    meta_description = models.CharField(max_length=150, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250, null=False, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True)
+    product_image = models.ImageField(upload_to='products/', null=True, blank=True)
+    description = models.TextField(max_length=600, null=False, blank=False)
+    quantity = models.PositiveIntegerField(null=False, blank=False)
+    original_price = models.FloatField(null=False, blank=False)
+    selling_price = models.FloatField(null=False, blank=False)
+    status = models.BooleanField(default=False, help_text="0=default, 1=Hidden")
+    trending = models.BooleanField(default=False, help_text="0=default, 1=Trending")
+    tag = models.CharField(max_length=150, null=False, blank=False)
+    meta_title = models.CharField(max_length=150, null=False, blank=False)
+    meta_keywords = models.CharField(max_length=150, null=False, blank=False)
+    meta_description = models.CharField(max_length=150, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_qty = models.PositiveIntegerField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
