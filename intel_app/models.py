4baeff5208432c728008cf7bbabb033682d2bf71
mikegyamfi <codeage20@gmail.com>
2024-01-22 23:58:50 +0000

    paystack

--- intel_app/models.py
+++ intel_app/models.py
@@ -260,6 +260,9 @@ class GeneralCategory(models.Model):
     name = models.CharField(max_length=250, null=False, blank=False)
     description = models.CharField(max_length=250, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=250, null=False, blank=False, default="Generic")
