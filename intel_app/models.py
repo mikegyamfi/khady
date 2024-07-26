53ec93918d54c9257ad964184540bc3433b1ccab
mikegyamfi <codeage20@gmail.com>
2024-07-26 21:02:57 +0000

    hi

--- intel_app/models.py
+++ intel_app/models.py
@@ -508,8 +508,8 @@ class Package(models.Model):
     order = models.ForeignKey(ShippingOrder, related_name='packages', on_delete=models.CASCADE)
     item_name = models.CharField(max_length=255, null=True, blank=True)
     quantity = models.IntegerField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=1,  null=True, blank=True)
-    cbm = models.DecimalField(max_digits=10, decimal_places=1,  null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=1, null=True, blank=True)
+    cbm = models.DecimalField(max_digits=8, decimal_places=5, null=True, blank=True)
     package_tracking_number = models.CharField(max_length=150, null=True, blank=True)
 
     def __str__(self):
