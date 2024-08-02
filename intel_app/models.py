595a175e0e2a94b00832cb2a12ac3f622c48beaf
mikegyamfi <codeage20@gmail.com>
2024-08-02 05:15:38 +0000

    hi

--- intel_app/models.py
+++ intel_app/models.py
@@ -509,7 +509,7 @@ class Package(models.Model):
     item_name = models.CharField(max_length=255, null=True, blank=True)
     quantity = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=5, null=True, blank=True)
-    cbm = models.DecimalField(max_digits=8, decimal_places=5, null=True, blank=True)
+    cbm = models.DecimalField(max_digits=10, decimal_places=6, null=True, blank=True)
     package_tracking_number = models.CharField(max_length=150, null=True, blank=True)
 
     def __str__(self):
